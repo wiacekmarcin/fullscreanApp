9367a7425210de90ed7634a23a723f43dfc44738
Marcin Wiacek <marcin.wiacek@transbit.com.pl>
2023-08-25 09:33:26 +0200

    Poprawki z widget lazienka

--- lazienka.py
+++ lazienka.py
@@ -1,3 +1,4 @@
+#*-* coding:utf-8 *-*
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -12,6 +13,7 @@ class Lazienka(blackwidget.BlackWidget):
         self.lazienkaUi = lazienka_ui.Ui_Lazienka()
         self.setupUi(self)
         self.firstTime = True
+        self.vals = {}
         self.lazienkaUi.etLazienka.setText("")
 
     def getRect(self):
@@ -21,27 +23,16 @@ class Lazienka(blackwidget.BlackWidget):
         self.lazienkaUi.setupUi(Lazienka)
 
     def receiveNotfication(self, dictValue):
-        vals['lazienka_temp'] = valg['ltemp']
-                vals['lazienka_humi'] = valg['lhum']
         if "lazienka_temp" in dictValue:
-            self.lazienkaUi. = dictValue["28fff708640400a3"]
-        if "286cf90f0b0000ae" in dictValue:
-            self.raspTemp = dictValue["286cf90f0b0000ae"]
+            self.lazienkaUi.temperature.setText(dictValue["lazienka_temp"])
+        if "lazienka_humi" in dictValue:
+            self.lazienkaUi.wilgotnosc.setText(dictValue["lazienka_humi"])
 
     def timeout(self, dt):
         hour = dt.time().hour()
         min = dt.time().minute()
         sec = dt.time().second()
-        self.statsUi.cpuSpeed.setText("%d MHz" % getCPUcurrentSpeed())
-        self.statsUi.cpuUsage.setText("%d %%" % getCPUuse())
         
-        if not self.firstTime and min % 5 != 0 or sec != 0:
-            return
+        
 
-        if self.firstTime or (min == 0 and sec == 0):
-            self.statsUi.localIp.setText("%s" % getIP())
-            self.statsUi.zewnetrzneIp.setText("%s" % getIPOut())
-        self.firstTime = False
-        self.statsUi.tempGPU.setText("%2.1f\u00B0C" % getCPUtemperature())
-        self.statsUi.tempMonitor.setText("%s\u00B0C" % self.monitorTemp)
-        self.statsUi.tempRasp.setText("%s\u00B0C" % self.raspTemp)
+
